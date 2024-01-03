6f1affc1d58252f71a07763a31b9e52d2fc9b554
Marwan <marwan.subhi2002@yahoo.com>
2024-01-02 20:19:13 -0500

    Added more classes

--- src/Test2.java
+++ src/Test2.java
@@ -1,7 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Marwan on 02 Jan, 2024 around 8:08 PM
  */
 public class Test2 {
 
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Marwan",38);
+        map.put("Manara",25);
+        map.put("Mahdi",10);
+        map.put("Majd",6);
+        System.out.println(map);
+
+
+
+    }
 
 }
